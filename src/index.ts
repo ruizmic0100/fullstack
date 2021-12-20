f5a0eb655a9958f7ce9a584cd66d8b8333cdf1ce
michael <ruizmic0100@hotmail.com>
2021-12-19 21:48:33 -0800

    Added a postgreSQL role and database. Added setup for MikroORM.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'ghostrunner',
+        user: 'ghostrunner',
+        password: 'GR-073B',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+
+
+
+};
+
+
+
+console.log("Hello heorld");
